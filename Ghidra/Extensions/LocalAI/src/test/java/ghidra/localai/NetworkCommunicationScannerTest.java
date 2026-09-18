f6e82a9135bf15dc6fae18d6322ff5c18e94859b
RabbitBreeder1 <155046452+RabbitBreeder1@users.noreply.github.com>
2026-09-18 14:52:28 -0500

    LocalAI: test network false-positive filtering

--- Ghidra/Extensions/LocalAI/src/test/java/ghidra/localai/NetworkCommunicationScannerTest.java
+++ Ghidra/Extensions/LocalAI/src/test/java/ghidra/localai/NetworkCommunicationScannerTest.java
@@ -37,4 +37,34 @@ public class NetworkCommunicationScannerTest {
             NetworkCommunicationScanner.classifyNetworkApi("KERNEL32.dll", "CreateFileW")
         );
     }
+
+    @Test
+    public void rejectsMicrosoftSchemaNamespaceUrl() {
+        String xml = "<?xml version=\"1.0\"?><assembly xmlns=\"urn:schemas-microsoft-com:asm.v1\">" +
+            "<windowsSettings xmlns=\"http://schemas.microsoft.com/SMI/2005/WindowsSettings\">";
+
+        org.junit.Assert.assertFalse(
+            NetworkCommunicationScanner.isUsefulUrl(
+                "http://schemas.microsoft.com/SMI/2005/WindowsSettings",
+                xml
+            )
+        );
+    }
+
+    @Test
+    public void rejectsSchemaDomain() {
+        org.junit.Assert.assertFalse(
+            NetworkCommunicationScanner.isUsefulDomain("schemas.microsoft.com")
+        );
+    }
+
+    @Test
+    public void keepsNormalGameEndpoint() {
+        org.junit.Assert.assertTrue(
+            NetworkCommunicationScanner.isUsefulUrl(
+                "https://auth.examplegame.com/v2/login",
+                "https://auth.examplegame.com/v2/login"
+            )
+        );
+    }
 }
