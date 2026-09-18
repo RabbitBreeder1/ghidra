a06ff79bdf2839bfedf098409a32c31910223bd1
RabbitBreeder1 <155046452+RabbitBreeder1@users.noreply.github.com>
2026-09-18 15:48:22 -0500

    LocalAI: add build ID and analysis readiness checks

---
+++ Ghidra/Extensions/LocalAI/src/main/java/ghidra/localai/LocalAIBuildInfo.java
@@ -0,0 +1,8 @@
+package ghidra.localai;
+
+public final class LocalAIBuildInfo {
+    public static final String BUILD_ID = "2026-09-18-preservation-r5";
+
+    private LocalAIBuildInfo() {
+    }
+}
